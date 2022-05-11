7c0cd663c197acba452b818455d9099857f08689
T.S. Foster <hi@tsf.io>
2022-05-11 14:18:16 +0100

    Initial commit

---
+++ strange-harmony-dark.js
@@ -0,0 +1,41 @@
+black = 'rgb(34, 34, 34)';
+red = 'rgb(238, 0, 0)';
+green = 'rgb(0, 138, 0)';
+yellow = 'rgb(125, 122, 0)';
+blue = 'rgb(0, 106, 255)';
+magenta = 'rgb(210, 0, 209)';
+cyan = 'rgb(0, 132, 132)';
+white = 'rgb(255, 255, 255)';
+lightBlack = 'rgb(0, 0, 0)';
+lightRed = 'rgb(180, 0, 0)';
+lightGreen = 'rgb(0, 103, 0)';
+lightYellow = 'rgb(96, 93, 0)';
+lightBlue = 'rgb(0, 79, 191)';
+lightMagenta = 'rgb(157, 0, 156)';
+lightCyan = 'rgb(0, 99, 99)';
+lightWhite = 'rgb(238, 238, 238)';
+
+t.prefs_.set('color-palette-overrides', [
+  black,
+  red,
+  green,
+  yellow,
+  blue,
+  magenta,
+  cyan,
+  white,
+  lightBlack,
+  lightRed,
+  lightGreen,
+  lightYellow,
+  lightBlue,
+  lightMagenta,
+  lightCyan,
+  lightWhite,
+]);
+
+t.prefs_.set('cursor-color', 'rgba(255, 255, 255, 0.5)');
+t.prefs_.set('cursor-blink', false);
+
+t.prefs_.set('foreground-color', white);
+t.prefs_.set('background-color', black);
